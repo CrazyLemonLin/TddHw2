c5089316a1a24cce57a8e6714a4b93d22d4c4507
CrazyLemonLin <lemonlinchju@hotmail.com>
2017-06-18 23:14:17 +0800

    init project

---
+++ HarryPotterShoppingCartTest/HarryPotterShoppingCartServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HarryPotterShoppingCartTest
+{
+    [TestClass]
+    public class HarryPotterShoppingCartServiceTest
+    {
+       
+    }
+}
