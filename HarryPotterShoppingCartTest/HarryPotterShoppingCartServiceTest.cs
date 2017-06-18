27298d3e8995df674ab9f193b271ef8b34854af2
CrazyLemonLin <lemonlinchju@hotmail.com>
2017-06-18 23:38:04 +0800

    加上第二個Scenario 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 測試不通過

--- HarryPotterShoppingCartTest/HarryPotterShoppingCartServiceTest.cs
+++ HarryPotterShoppingCartTest/HarryPotterShoppingCartServiceTest.cs
@@ -10,8 +10,8 @@ namespace HarryPotterShoppingCartTest
     [TestFixture]
     public class HarryPotterShoppingCartServiceTest
     {
-       [Test]
-       public void CheckOutTest_只買了第一集1本_結帳價格應為100()
+        [Test]
+        public void CheckOutTest_只買了第一集1本_結帳價格應為100()
         {
             //arrange
             var target = new HarryPotterShoppingCartService();
@@ -24,6 +24,33 @@ namespace HarryPotterShoppingCartTest
                 }
             };
             var expected = 100;
+            //act
+            int result = target.CheckOut(shoppingList);
+
+            //assert
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void CheckOutTest_只買了第一集第二集各1本_結帳價格應為190()
+        {
+            //arrange
+            var target = new HarryPotterShoppingCartService();
+            List<FantasyNovel> shoppingList = new List<FantasyNovel> {
+                new FantasyNovel
+                {
+                    Id =1,
+                    Name="HarryPotterBook1",
+                    UnitPrice=100
+                },
+                new FantasyNovel
+                {
+                    Id =2,
+                    Name="HarryPotterBook2",
+                    UnitPrice=100
+                }
+            };
+            var expected = 190;
             //act
             int result = target.CheckOut(shoppingList);
 
